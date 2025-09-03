71f46c246f38ad33375c8e82a2883c0254cd9d9c
Greg Nishihara <gnishihara@gmail.com>
2025-09-04 08:49:14 +0900

    Update

--- template.R
+++ template.R
@@ -12,6 +12,11 @@ library(janitor)
 library(furrr)
 library(tidymodels)
 library(gnnlab)
+# library(tidymodels)
+# library(furrr)
+
+# Read xlsx files
+# library(readxl)
 
 # For ecological data analysis
 # library(vegan)
@@ -20,11 +25,16 @@ library(gnnlab)
 # For NEP data analysis
 # library(marelac)
 # library(suntools)
+# library(seacarb)
 
 # For Bayesian modeling.
 # library(brms)
 # library(tidybayes)
 # library(emmeans)
+
+# For GAM
+# library(mgcv)
+# library(gratia)
 
 # For plotting and image handling
 library(ggpubr)
@@ -53,11 +63,11 @@ theme_replace(
   panel.background = element_rect(fill = "white", color = "black", linewidth = 1),
   axis.line.x = element_blank(),
   axis.line.y = element_blank(),
-  axis.title.x = element_markdown(family = "notosans", lineheight = 1.5, margin = unit(c(0.5, 0, 0, 0), "lines")),
-  axis.title.y = element_markdown(family = "notosans", angle = 90, linewidth = 1.5, margin = unit(c(0, 0.5, 0, 0), "lines")),
+  axis.title.x = element_markdown(family = "notosans-jp", lineheight = 1.5, margin = unit(c(0.5, 0, 0, 0), "lines")),
+  axis.title.y = element_markdown(family = "notosans-jp", angle = 90, linewidth = 1.5, margin = unit(c(0, 0.5, 0, 0), "lines")),
   legend.background = element_blank(),
-  legend.title = element_markdown(family = "notosans"),
-  legend.text = element_markdown(family = "notosans", size = 8)
+  legend.title = element_markdown(family = "notosans-jp"),
+  legend.text = element_markdown(family = "notosans-jp", size = 8)
 )
 # Automatically use the showtext library to render fonts in plots.
 showtext_auto()
@@ -73,8 +83,10 @@ save_plots <- function(filename, plot = last_plot(), width = 80, height = 80, un
   #   Font size should be 2.5 to 3.5 mm (8 to 10 pt)
   pdfname <- tools::file_path_sans_ext(basename(fname)) |> str_c(".pdf")
   pngname <- str_replace(pdfname, "pdf", "png")
+  tifname <- str_replace(pdfname, "pdf", "tif")
   ggsave(pdfname, plot = plot, width = width, height = height, density = density)
   image_read_pdf(pdfname, density = density) |> image_write(pngname)
+  image_read_pdf(pdfname, density = density) |> image_write(tifname)
 }
 
 
